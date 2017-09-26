f6597b2eeecf1f003a570f1ef15ad21202873234
luis <luis@tecneca.com>
2017-09-27 00:30:04 +0200

    - map options

--- src/pages/mapa/mapa.ts
+++ src/pages/mapa/mapa.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
-import { GoogleMaps, GoogleMap, GoogleMapsEvent } from '@ionic-native/google-maps';
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions } from '@ionic-native/google-maps';
 
 
 @IonicPage()
@@ -39,10 +39,21 @@ export class MapaPage {
 
   loadMap() {
 
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+
     this.mapElement = document.getElementById('map');
 
     //this.map = new GoogleMap(this.mapElement);
-    this.map = this.googleMaps.create(this.mapElement);
+    this.map = this.googleMaps.create(this.mapElement, mapOptions);
 
     this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
 
