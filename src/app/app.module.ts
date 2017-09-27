7c2d572f34c721334d9c7725f765d73c8b0da6e5
luis <luis@tecneca.com>
2017-09-27 20:55:47 +0200

    - añade plugin geolocation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { GoogleMaps, Spherical } from '@ionic-native/google-maps'
+import { Geolocation } from '@ionic-native/geolocation';
 
 import { MyApp } from './app.component';
-
-import {GoogleMaps, Spherical} from '@ionic-native/google-maps'
 
 @NgModule({
   declarations: [MyApp],
@@ -24,6 +25,7 @@ import {GoogleMaps, Spherical} from '@ionic-native/google-maps'
     SplashScreen,
     GoogleMaps,
     Spherical,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
